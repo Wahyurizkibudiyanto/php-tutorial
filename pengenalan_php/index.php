722a7edb5ce7634b08d8e5c243482350dba9de34
wahyurizkibudiyanto <wahyudasilfa@gmail.com>
2025-08-03 21:44:24 +0700

    belajar hari ke 2

--- pengenalan_php/index.php
+++ pengenalan_php/index.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pengenalan Materi</title>
 </head>
-
 <body>
     <h1><?php echo "pengenalan materi php" ?></h1>
     <h1><?= "pengenalan materi php" ?></h1>
