2d2313c349d11f936091cc9c3c468bfe1aaf81dd
wahyurizkibudiyanto <wahyudasilfa@gmail.com>
2025-08-05 21:00:14 +0700

    blj 3

---
+++ tipe-data/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipe Data</title>
+</head>
+
+<body>
+    <?php
+    $salam = "Hellow Alex"; ?>
+    <h1><?php echo $salam; ?></h1>
+
+    <!-- cara cek tipe data -->
+    <h1><?php echo gettype($salam); ?></h1>
+</body>
+
+</html>
